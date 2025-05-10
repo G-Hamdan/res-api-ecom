fbf2169453296fa2306b13c104da4162c9e8cc85
G-Hamdan <94384827+G-Hamdan@users.noreply.github.com>
2025-05-10 10:05:32 +0200

    agdefswbhiluaglfihuksdfa

--- frontend/src/pages/LogInPage.jsx
+++ frontend/src/pages/LogInPage.jsx
@@ -12,7 +12,7 @@ const LogInPage = ({ setIsLoggedIn }) => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:3000/api/users/login", {
+      const response = await fetch("https://hardcoregear.netlify.app/api/users/login", {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password })
